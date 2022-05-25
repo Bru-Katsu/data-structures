826f37da9c05216e236bd8ff96b3472948cc34c6
Bruno <bruno.alves5009@outlook.com.br>
2022-05-24 22:19:54 -0300

    Adicionado estrutura de fila

---
+++ DataStructures.Tests/QueueTests.cs
@@ -0,0 +1,149 @@
+﻿using DataStructures.Listas;
+using System;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class QueueTests
+    {
+        [Fact(DisplayName = "Ao espiar, deve retornar o primeiro elemento da fila")]
+        [Trait("Queue", "Espiar")]
+        public void Espiar_FilaComItens_DeveRetornarPrimeiroElementoDaFila()
+        {
+            //arrange
+            var fila = new Queue<int>();
+            var valor = 2;
+
+            fila.Enqueue(valor);
+            fila.Enqueue(1);
+
+            //act
+            var observado = fila.Peek();
+
+            //assert
+            Assert.Equal(valor, observado);
+        }
+
+        [Fact(DisplayName = "Ao enfileirar, o tamanho da fila deve ser incrementado")]
+        [Trait("Queue", "Enfileirar")]
+        public void Enfileirar_NovaFila_DeveIncrementarTamanho()
+        {
+            //arrange
+            var fila = new Queue<int>();
+
+            //act
+            fila.Enqueue(1);
+            fila.Enqueue(2);
+            fila.Enqueue(3);
+
+            //assert
+            Assert.Equal(3, fila.Size);
+        }
+
+        [Fact(DisplayName = "Ao enfileirar, a fila não pode ser vazia")]
+        [Trait("Queue", "Enfileirar")]
+        public void Enfileirar_NovaFila_NaoPodeSerVazia()
+        {
+            //arrange
+            var fila = new Queue<int>();
+
+            //act
+            fila.Enqueue(1);
+
+            //assert
+            Assert.False(fila.IsEmpty);
+        }
+
+        [Fact(DisplayName = "Ao desenfileirar, deve retornar o primeiro elemento adicionado")]
+        [Trait("Queue", "Desenfileirar")]
+        public void Desenfileirar_FilaComItens_DeveRetornarPrimeiroElementoDaFila()
+        {
+            //arrange
+            var fila = new Queue<int>();
+
+            var valor1 = 1;
+            var valor2 = 2;
+
+            fila.Enqueue(valor2);
+            fila.Enqueue(valor1);
+
+            //act
+            var primeiroDesenfileirar = fila.Dequeue();
+            var segundoDesenfileirar = fila.Dequeue();
+
+            //assert
+            Assert.Equal(valor2, primeiroDesenfileirar);
+            Assert.Equal(valor1, segundoDesenfileirar);
+        }
+
+        [Fact(DisplayName = "Ao desenfileirar todos os itens, a fila deve estar vazia")]
+        [Trait("Queue", "Desenfileirar")]
+        public void Desenfileirar_FilaComItens_DeveEstarVazia()
+        {
+            //arrange
+            var fila = new Queue<int>();
+
+            var valor1 = 1;
+            var valor2 = 2;
+
+            fila.Enqueue(valor2);
+            fila.Enqueue(valor1);
+
+            //act
+            fila.Dequeue();
+            fila.Dequeue();
+
+            //assert
+            Assert.True(fila.IsEmpty);            
+        }
+
+        [Fact(DisplayName = "Ao criar uma fila com a capacidade negativa, deve estourar exception")]
+        [Trait("Queue", "Instânciar")]
+        public void Instanciar_FilaComCapacidadeNegativa_DeveEstourarException()
+        {
+            //arrange
+            var capacidade = -1;
+
+            //act & assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Queue<int>(capacidade));
+        }
+
+        [Fact(DisplayName = "Ao criar uma fila, o tamanho deve ser zero")]
+        [Trait("Queue", "Instânciar")]
+        public void Instanciar_FilaSemItens_TamanhoDeveSerZero()
+        {
+            //arrange & act
+            var filaSemCapacidade = new Queue<int>();
+            var filaComCapacidade = new Queue<int>(5);
+
+            //assert
+            Assert.Equal(0, filaSemCapacidade.Size);
+            Assert.Equal(0, filaComCapacidade.Size);
+        }
+
+        [Fact(DisplayName = "Ao criar um array a partir da fila, deve respeitar a mesma ordem interna")]
+        [Trait("Queue", "ToArray")]
+        public void ConverterArray_FilaComItens_DeveTerAMesmaSequenciaDeItens()
+        {
+            //arrange
+            var fila = new Queue<int>();
+
+            fila.Enqueue(1);
+            fila.Enqueue(2);
+            fila.Enqueue(3);
+            fila.Enqueue(4);
+            fila.Enqueue(5);
+            fila.Enqueue(6);
+
+            //act
+            var arr = fila.ToArray();
+
+            //assert
+            for (int index = 0; index < fila.Size; index++)
+            {
+                var value = fila.Dequeue();
+                Assert.Equal(value, arr[index]);
+            }
+        }
+    }
+}
---
+++ DataStructures/Listas/Queue.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace DataStructures.Listas
+{
+    [ComVisible(true)]
+    [Serializable]
+    public class Queue<T> : IEnumerable<T>
+    {
+        public Queue()
+        {
+            _storage = new T[_defaultCapacity];
+        }
+
+        public Queue(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException("Negative capacity is not supported!");
+
+            _storage = new T[capacity];
+            _hasCapacityDefined = true;
+        }
+
+        private const int _minimumGrow = 2;
+        private const int _defaultCapacity = 10;
+
+        private readonly bool _hasCapacityDefined = false;
+        
+        private int _internalSize = 0;
+        private T[] _storage;
+
+        private int _head;
+        private int _tail;
+
+        public int Size => _internalSize;
+        public bool IsEmpty => Size == 0;
+
+        //O(1)
+        public T Peek()
+        {
+            if(IsEmpty)
+                return default;
+
+            return _storage[_head];
+        }
+
+        //O(n) only on expand, otherwise is O(1)
+        public void Enqueue(T element)
+        {
+            if (_internalSize == _storage.Length)
+            {
+                if(_hasCapacityDefined)
+                    throw new ArgumentOutOfRangeException("The Queue is full!");
+                else
+                    Expand();
+            }
+
+            _storage[_tail] = element;
+            _tail++;
+            _internalSize++;
+        }
+
+        //O(1)
+        public T Dequeue()
+        {
+            if (IsEmpty)
+                throw new ArgumentOutOfRangeException("Queue is Empty!");
+
+            T dequeued = _storage[_head];
+            _storage[_head] = default;
+            _head = (_head + 1) % _storage.Length;
+
+            _internalSize--;
+
+            return dequeued;
+        }
+
+        //O(n)
+        public void Trim() => Redimensionate(_internalSize);
+
+        //O(n)
+        private void Expand()
+        {
+            int newCapacity = (int)((long)_storage.Length * 2 / 100);
+            if (newCapacity < _storage.Length + _minimumGrow)
+            {
+                newCapacity = _storage.Length + _minimumGrow;
+            }
+
+            Redimensionate(newCapacity);
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[_internalSize];
+            Array.Copy(_storage, array, _internalSize);
+
+            return array;
+        }
+
+        //O(n)
+        private void Redimensionate(int toCapacity)
+        {
+            var newStorage = new T[toCapacity];
+            if (_internalSize > 0)
+            {
+                if (_head < _tail)
+                    Array.Copy(_storage, newStorage, _internalSize);                    
+                else
+                {
+                    Array.Copy(_storage, _head, newStorage, 0, _storage.Length - _head);
+                    Array.Copy(_storage, 0, newStorage, _storage.Length - _head, _tail);
+                }
+            }
+
+            _storage = newStorage;
+            _head = 0;
+            _tail = (_internalSize == toCapacity) ? 0 : _internalSize;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _storage.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _storage.GetEnumerator();
+        }
+    }
+}
