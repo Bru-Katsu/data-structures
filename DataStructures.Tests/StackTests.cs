c50223c68ae6d6e891992e7d6e80ce9eab4ceec0
Bruno <bruno.alves5009@outlook.com.br>
2022-05-24 22:19:05 -0300

    Melhorias na estrutura Stack

---
+++ DataStructures.Tests/StackTests.cs
@@ -0,0 +1,134 @@
+﻿using DataStructures.Listas;
+using System;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class StackTests
+    {
+        [Fact(DisplayName = "Ao empilhar, o elemento deve estar no topo")]
+        [Trait("Stack", "Empilhar")]
+        public void Empilhar_PilhaVazia_ElementoDeveEstarNoTopo()
+        {
+            //arrange
+            var pilha = new Stack<int>();
+            var valor = 2;
+
+            //act
+            pilha.Push(1);
+            pilha.Push(valor);
+
+            //assert
+            Assert.Equal(valor, pilha.Peek());
+        }
+
+        [Fact(DisplayName = "Tamanho da pilha deve ser igual a quantidade de empilhamentos")]
+        [Trait("Stack", "Empilhar")]
+        public void Empilhar_PilhaComItens_TamanhoDeveSerIgualAQuantidadeDeEmpilhamentos()
+        {
+            //arrange
+            var tamanho = 5;
+            var pilha = new Stack<int>(tamanho);
+            var randomizer = new Random();
+
+            //act
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+
+            //assert            
+            Assert.Equal(tamanho, pilha.Size);
+        }
+
+        [Fact(DisplayName = "Ao Empilhar mais itens do que o tamanho definido, deve estourar exception")]
+        [Trait("Stack", "Empilhar")]        
+        public void Empilhar_PilhaComTamanho_DeveEstourarExceptionAoExceder()
+        {
+            //arrange
+            var tamanho = 4;
+            var pilha = new Stack<int>(tamanho);
+            var randomizer = new Random();
+
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+
+            //act
+            Assert.Throws<ArgumentOutOfRangeException>(() => pilha.Push(randomizer.Next()));
+        }
+
+        [Fact(DisplayName = "Ao tentar desempilhar uma pilha vazia, deve estourar exception")]
+        [Trait("Stack", "Desempilhar")]
+        public void Desempilhar_PilhaVazia_DeveEstourarException()
+        {
+            //arrange
+            var pilha = new Stack<int>();
+
+            //act & assert
+            Assert.Throws<InvalidOperationException>(() => pilha.Pop());
+        }
+
+        [Fact(DisplayName = "Ao desempilhar o último elemento, a pilha deve estar vazia")]
+        [Trait("Stack", "Desempilhar")]
+        public void Desempilhar_PilhaComUmItem_DeveFicarVazia()
+        {
+            //arrange
+            var pilha = new Stack<int>();
+            pilha.Push(1);
+
+            //act
+            pilha.Pop();
+
+            //assert
+            Assert.True(pilha.IsEmpty);
+        }
+
+        [Fact(DisplayName = "Ao desempilhar, deve retornar o último elemento da pilha")]
+        [Trait("Stack", "Desempilhar")]
+        public void Desempilhar_PilhaComItens_DeveRetornarUltimoItem()
+        {
+            //arrange
+            var pilha = new Stack<int>();
+            var randomizer = new Random();
+
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+            pilha.Push(randomizer.Next());
+
+            var ultimoElemento = pilha.Peek();
+
+            //act
+            var elemento = pilha.Pop();
+
+            //assert
+            Assert.Equal(ultimoElemento, elemento);
+        }
+
+        [Fact(DisplayName = "Ao criar uma pilha com a capacidade negativa, deve estourar exception")]
+        [Trait("Stack", "Instânciar")]
+        public void Instanciar_PilhaComCapacidadeNegativa_DeveEstourarException()
+        {
+            //arrange
+            var capacidade = -1;
+
+            //act & assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Stack<int>(capacidade));            
+        }
+
+        [Fact(DisplayName = "Ao criar uma pilha sem itens, o tamanho deve ser igual a zero")]
+        [Trait("Stack", "Instânciar")]
+        public void Instanciar_PilhaSemItens_TamanhoDeveSerZero()
+        {
+            //arrange
+            var pilha = new Stack<int>();
+
+            //act & assert
+            Assert.Equal(0, pilha.Size);
+        }
+    }
+}
---
+++ DataStructures/Listas/Stack.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DataStructures.Listas
+{
+    [ComVisible(true)]
+    [Serializable]
+    public class Stack<T>
+    {
+        public Stack()
+        {
+            _storage = new T[_defaultCapacity];
+        }
+
+        public Stack(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException("Negative capacity is not supported!");
+
+            _hasCapacityDefined = true;
+            _storage = new T[capacity];
+        }
+
+        private const int _defaultCapacity = 10;
+        private const int _growthFactor = 2;
+
+        private readonly bool _hasCapacityDefined = false;
+        
+        private int _internalSize = -1;
+        private T[] _storage;
+
+        public int Size => _internalSize + 1;
+        public bool IsEmpty => Size == 0;
+
+        //O(1)
+        public T Peek()
+        {
+            if (!IsEmpty)
+                return _storage[_internalSize];
+
+            return default;
+        }
+
+        //O(n) only on resize, otherwise is O(1)
+        public void Push(T element)
+        {
+            if (Size == _storage.Length)
+            {
+                if (_hasCapacityDefined)
+                    throw new ArgumentOutOfRangeException("The Stack is full!");
+                else
+                    Array.Resize(ref _storage, _growthFactor * _storage.Length);
+            }
+
+            _internalSize++;
+            _storage[_internalSize] = element;
+        }
+
+        //O(1)
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("The Stack is empty!");
+
+            var lastElement = _storage[_internalSize];
+            _storage[_internalSize] = default;
+
+            _internalSize--;
+
+            return lastElement;
+        }
+    }
+}
