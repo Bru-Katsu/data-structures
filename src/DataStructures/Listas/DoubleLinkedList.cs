730a2b2854f33d6f7663b8350bc7a06936b18feb
Bruno Alves <bruno.alves5009@gmail.com>
2023-01-19 00:22:11 -0300

    Adicionado DoubleLinkedList

---
+++ src/DataStructures/Listas/DoubleLinkedList.cs
@@ -0,0 +1,258 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+
+namespace DataStructures.Listas
+{
+    [ComVisible(true)]
+    public class DoubleLinkedList<T> : IEnumerable<T>
+    {
+        private DoubleLinkedNode<T> _head;
+        private DoubleLinkedNode<T> _tail;
+        private int _lenght;
+
+        /// <summary>
+        /// Obtém ou insere valores na lista
+        /// </summary>
+        /// <param name="position">posição</param>
+        /// <returns><see cref="T"/></returns>
+        public T this[int position]
+        {
+            get => GetValueAt(position);
+            set => InsertAt(position, value);
+        }
+
+        public int Count => _lenght;
+
+        public DoubleLinkedList() { }
+        public DoubleLinkedList(T value)
+        {
+            Initialize(value);
+        }
+
+        /// <summary>
+        /// <para>Adiciona um valor ao final da lista</para>
+        /// <para>Complexidade de O(1)</para>
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddLast(T value)
+        {
+            if (_tail == null)
+            {
+                Initialize(value);
+                return;
+            }
+
+            var newNode = new DoubleLinkedNode<T>(value, _tail);
+
+            _tail.SetNext(newNode);
+            _tail = _tail.Next;
+
+            _lenght++;
+        }
+
+        /// <summary>
+        /// <para>Adiciona um valor no início da lista</para>
+        /// <para>Complexidade de O(1)</para>
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddFirst(T value)
+        {
+            if (_head == null)
+            {
+                Initialize(value);
+                return;
+            }
+
+            var newNode = new DoubleLinkedNode<T>(value);
+            newNode.SetNext(_head);
+            _head.SetPrevious(newNode);
+            _head = newNode;
+
+            _lenght++;
+        }
+
+        /// <summary>
+        /// <para>Adiciona um valor na posição informada</para>
+        /// <para>Complexidade de O(n)</para>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void InsertAt(int position, T value)
+        {
+            ThrowIfGreaterThanPositionsRange(position);
+
+            if (_head == null)
+                Initialize(value);
+            else if (position == 0)
+                AddFirst(value);
+            else if (position == _lenght)
+                AddLast(value);
+            else
+            {
+                DoubleLinkedNode<T> previousNode = TraverseTo(position - 1);
+                DoubleLinkedNode<T> currentNode = previousNode.Next;
+
+                var newNode = new DoubleLinkedNode<T>(value);
+
+                previousNode?.SetNext(newNode);
+
+                newNode.SetPrevious(previousNode);
+                newNode.SetNext(currentNode);
+
+                currentNode.SetPrevious(newNode);
+
+                _lenght++;
+            }
+        }
+
+        /// <summary>
+        /// <para>Remove valor na posição informada</para>
+        /// <para>Complexidade de O(n)</para>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void RemoveAt(int position)
+        {
+            ThrowIfNotInPositionsRange(position);
+
+            DoubleLinkedNode<T> previousNode = TraverseTo(position - 1);
+            DoubleLinkedNode<T> currentNode = previousNode.Next;
+
+            previousNode?.SetNext(currentNode.Next);
+            currentNode.Next.SetPrevious(previousNode);
+
+            _lenght--;
+        }
+
+        /// <summary>
+        /// <para>Busca valor na posição informada</para>
+        /// <para>Complexidade de O(n)</para>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public T GetValueAt(int position)
+        {
+            ThrowIfNotInPositionsRange(position);
+
+            if (position == 0 || _head.Next == null)
+                return _head.Value;
+
+            var currentNode = TraverseTo(position);
+
+            return currentNode.Value;
+        }
+
+        public void Add(T item)
+        {
+            AddLast(item);
+        }
+
+        #region Enumerable
+        private class DoubleLinkedListEnumerator<Type> : IEnumerator<Type>
+        {
+            private readonly DoubleLinkedNode<Type> _head;
+            private DoubleLinkedNode<Type> _current = default;
+
+            public DoubleLinkedListEnumerator(DoubleLinkedNode<Type> head)
+            {
+                _head = head;
+            }
+
+            public Type Current => _current.Value;
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                _current = null;
+            }
+
+            public bool MoveNext()
+            {
+                _current = _current is null ? _head : _current.Next;
+
+                if (_current == null)
+                    return false;
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                _current = default;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new DoubleLinkedListEnumerator<T>(_head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new DoubleLinkedListEnumerator<T>(_head);
+        }
+        #endregion
+
+        private void Initialize(T value)
+        {
+            _head = new DoubleLinkedNode<T>(value);
+            _tail = _head;
+            _lenght++;
+        }
+
+        private DoubleLinkedNode<T> TraverseTo(int position)
+        {
+            var currentNode = _head;
+
+            for (int i = 1; i <= position; i++)
+                currentNode = currentNode.Next;
+
+            return currentNode;
+        }
+
+        private void ThrowIfGreaterThanPositionsRange(int position)
+        {
+            if (position < 0)
+                throw new ArgumentOutOfRangeException(nameof(position), "Não é permitido posições negativas!");
+
+            if (position > _lenght)
+                throw new ArgumentOutOfRangeException(nameof(position), "Posição fora do tamanho da lista!");
+        }
+
+        private void ThrowIfNotInPositionsRange(int position)
+        {
+            if (position < 0)
+                throw new ArgumentOutOfRangeException(nameof(position), "Não é permitido posições negativas!");
+
+            if (position > _lenght - 1)
+                throw new ArgumentOutOfRangeException(nameof(position), "Posição fora do tamanho da lista!");
+        }
+
+        private class DoubleLinkedNode<Type>
+        {
+            public DoubleLinkedNode(Type value)
+            {
+                Value = value;
+            }
+
+            public DoubleLinkedNode(Type value, DoubleLinkedNode<Type> previous) : this(value)
+            {
+                Previous = previous;
+            }
+
+            public DoubleLinkedNode<Type>? Previous { get; private set; }
+            public void SetPrevious(DoubleLinkedNode<Type> node)
+            {
+                Previous = node;
+            }
+
+            public DoubleLinkedNode<Type>? Next { get; private set; }
+            public void SetNext(DoubleLinkedNode<Type> node)
+            {
+                Next = node;
+            }
+
+            public Type Value { get; }
+        }
+    }
+}
---
+++ tests/DataStructures.Tests/DoubleLinkedListTests.cs
@@ -0,0 +1,185 @@
+﻿
+using DataStructures.Listas;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class DoubleDoubleLinkedListTests
+    {
+        #region Add Last
+        [Fact(DisplayName = "Ao adicionar o item, deve estar ao final da lista"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.AddLast))]
+        public void AdicionarAoFinal_ItemDeveEstarNoUltimoNode()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>()
+            {
+                1, 15
+            };
+
+            //act
+            list.AddLast(16);
+            var retorno = list.GetValueAt(list.Count - 1);
+
+            //assert            
+            Assert.Equal(16, retorno);
+        }
+
+        [Fact(DisplayName = "Ao adicionar o item, deve incrementar o tamanho"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.AddLast))]
+        public void AdicionarAoFinal_TamanhoDeveSerIncrementado()
+        {
+            //arrange
+            var list = new LinkedList<int>()
+            {
+                1, 15
+            };
+
+            //act
+            list.AddLast(16);
+            list.AddLast(17);
+
+            //assert            
+            Assert.Equal(4, list.Count);
+        }
+        #endregion
+
+        #region Add First
+        [Fact(DisplayName = "Ao adicionar o item, deve estar ao inicio da lista"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.AddFirst))]
+        public void AdicionarAoInicio_ItemDeveEstarNoPrimeiroNode()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 1, 15 };
+
+            //act
+            list.AddFirst(16);
+
+            var retorno = list.GetValueAt(0);
+
+            //assert            
+            Assert.Equal(16, retorno);
+        }
+
+        [Fact(DisplayName = "Ao adicionar o item, deve incrementar o tamanho"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.AddFirst))]
+        public void AdicionarAoInicio_TamanhoDeveSerIncrementado()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>()
+            {
+                1, 15
+            };
+
+            //act
+            list.AddFirst(16);
+            list.AddFirst(17);
+
+            //assert            
+            Assert.Equal(4, list.Count);
+        }
+        #endregion
+
+        #region Insert
+        [Theory(DisplayName = "Ao inserir o item, deve estar na posição definida"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.InsertAt))]
+        [InlineData(1, 20)]
+        [InlineData(2, 140)]
+        [InlineData(0, 100)]
+        [InlineData(3, 99)]
+        public void Inserir_ItemDeveEstarNoPosicaoDefinida(int posicao, int valor)
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act
+            list.InsertAt(posicao, valor);
+            var retorno = list.GetValueAt(posicao);
+
+            //assert            
+            Assert.Equal(valor, retorno);
+        }
+
+        [Fact(DisplayName = "Ao inserir o item em uma posição maior do que a última possível, deve retornar exception"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.InsertAt))]
+        public void Inserir_PosicaoMaiorQueAdicionarEmUltimaPosicao_DeveRetornarException()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act & assert                      
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.InsertAt(4, 99));
+        }
+
+        [Fact(DisplayName = "Ao inserir o item em uma posição negativa, deve retornar exception"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.InsertAt))]
+        public void Inserir_PosicaoNegativa_DeveRetornarException()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act & assert                      
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.InsertAt(-2, 99));
+        }
+
+        [Fact(DisplayName = "Ao inserir o item em uma posição válida, o tamanho deve ser incrementado"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.InsertAt))]
+        public void Inserir_PosicaoValida_DeveIncrementarTamanho()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act 
+            list.InsertAt(1, 50);
+            list.InsertAt(0, 99);
+
+            //assert
+            Assert.Equal(5, list.Count);
+        }
+        #endregion
+
+        #region Remove
+        [Fact(DisplayName = "Ao remover o item em uma posição maior do que a última, deve retornar exception"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.RemoveAt))]
+        public void Remover_PosicaoMaiorQueAdicionarEmUltimaPosicao_DeveRetornarException()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act & assert                      
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.InsertAt(4, 99));
+        }
+
+        [Fact(DisplayName = "Ao remover o item em uma posição negativa, deve retornar exception"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.RemoveAt))]
+        public void Remover_PosicaoNegativa_DeveRetornarException()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act & assert                      
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.RemoveAt(-1));
+        }
+
+        [Fact(DisplayName = "Ao remover um item em uma posição válida, o tamanho deve ser decrementado"), Trait(nameof(DoubleLinkedList<int>), nameof(DoubleLinkedList<int>.RemoveAt))]
+        public void Remover_PosicaoValida_DeveDecrementarTamanho()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 16, 1, 15 };
+
+            //act 
+            list.RemoveAt(1);
+
+            //assert
+            Assert.Equal(2, list.Count);
+        }
+        #endregion
+
+        [Fact(DisplayName = "Ao percorrer o IEnumerable, deve retornar o valor se existir na lista"), Trait(nameof(DoubleLinkedList<int>), "IEnumerable")]
+        public void Pesquisar_IEnumerableDeveRetornarItemCorreto()
+        {
+            //arrange
+            var list = new DoubleLinkedList<int>() { 0, 1, 2, 3 };
+            list.AddLast(1);
+            list.AddLast(15);
+            list.AddFirst(16);
+
+            //act & assert            
+            Assert.Equal(16, list.FirstOrDefault(x => x.Equals(16)));
+            Assert.Equal(15, list.FirstOrDefault(x => x.Equals(15)));
+            Assert.Equal(1, list.FirstOrDefault(x => x.Equals(1)));
+        }
+    }
+}
